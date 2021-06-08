652fa29506e0a6d2e4d05b207e1018cb2f2b1840
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-08 15:57:07 +0200

    Feat: Modelo, Controllador y Rutas para user

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+const User = require("../models/user");
+
+exports.registerUser = (req, res, next) => {
+    const {fullName, username, email, phoneNumber, password, gender} = req.body;
+
+    console.log(req.body, "lo que sea");
+
+    //TODO: Check propiedades!
+    if(!fullName?.trim()) {
+        throw new Error("Wrong params");
+    }
+
+    const user = new User({fullName, username, email, phoneNumber, password, gender});
+
+    return user.save().then( (createdUser) => {
+        res.status(201).json({message: "User registered!", 
+                              user: createdUser});
+        return createdUser;
+    });
+
+}
---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { GENDER_ENUMS } = require('../utils/constants');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+
+    username: {
+        type: String,
+        required: true
+      },
+
+    email: {
+      type: String,
+      required: true,
+    },
+
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    gender: {
+        type: String,
+        required: true,
+        enum: GENDER_ENUMS,
+    }
+ },
+ { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
