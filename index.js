92cf0bcb7955c6700f31ac62084d4d2611976488
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-08 21:44:39 +0200

    Refactor: Establecere headers de la peticion

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user");
+const serviceRoutes = require("./routes/service");
 const mongoose = require("mongoose");
 
 const app = express();
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Acess-Controll-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-      'Acess-Controll-Allow-Methods',
+      'Access-Control-Allow-Methods',
       'GET, POST, PUT, PATCH, DELETE'
     );
     res.header(
-      'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
     );
     next();
   });
 
 app.use("/user", userRoutes, bodyParser);
+app.use("/service", serviceRoutes, bodyParser);
 
 mongoose.connect(
     "mongodb+srv://srAdrover:1234@cluster0.wlvhj.mongodb.net/eventcoin?retryWrites=true&w=majority",
