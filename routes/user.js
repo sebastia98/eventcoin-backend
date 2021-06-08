cf4fccc502c27bff621bb4802a547f5cd04998a2
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-08 21:45:31 +0200

    Feat: añadir punto y coma

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ const userController = require("../controllers/user");
 
 const router = express.Router();
 
-router.post("/insertUser", userController.registerUser)
+router.post("/insertUser", userController.registerUser);
 
 module.exports = router;
