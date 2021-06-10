c8c92697b277538e03834fe501ea2f832906b9bb
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-10 19:40:15 +0200

    Feat: Comprobar si credenciales ya existes y encryptar password

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,28 @@
 const User = require("../models/user");
+const bcrypt = require('bcrypt');
 
-exports.registerUser = (req, res, next) => {
+exports.registerUser = (req, res, next) => 
+new Promise((resolve, reject) => {
+
     const {fullName, username, email, phoneNumber, password, gender} = req.body;
-
-    console.log(req.body, " => user");
 
     //TODO: Check propiedades!
     if(!fullName?.trim()) {
         throw new Error("Wrong params");
     }
 
-    const user = new User({fullName, username, email, phoneNumber, password, gender});
-
-    return user.save().then( (createdUser) => {
-        res.status(201).json({message: "User registered!", 
-                              user: createdUser});
-        return createdUser;
-    });
-
-}
+    User.findOne({$or : [{email}, {phoneNumber}, {username}]})
+        .then(userDoc => {
+            if(userDoc) {
+                throw new Error("Wrong params");
+            }
+            return bcrypt.hash(password, 12);
+        })
+        .then(hashedPassword => {
+            const user = new User({fullName, username, email, phoneNumber, password : hashedPassword, gender});
+            return user.save();    
+        })
+        .then((createdUser) => createdUser)
+        .then(user => resolve(res.status(201).json({message: "User registered!", user})))
+        .catch(({message}) => reject(res.status(500).json({message, ok : false})));
+})
