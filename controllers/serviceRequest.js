2fe92c669b944d88c4fd871c155b17c59c56052f
Sebas Adrover <sebastia.adrover@melia.com>
2021-09-16 17:16:21 +0200

    Refactor: Add conditions

--- controllers/serviceRequest.js
+++ controllers/serviceRequest.js
@@ -1,26 +1,68 @@
 const ServiceRequest = require("../models/serviceRequest");
+const User = require("../models/user")
+const Service = require("../models/service");
+const serviceRequest = require("../models/serviceRequest");
+const { request } = require("express");
 
-exports.registerRequestService = (req, res, next) => {
+exports.registerRequestService = (req, res, next) => {  
+    const serviceRequest = new ServiceRequest({...req.body});
+        isValidRequestService(serviceRequest)
+            .then(() => serviceRequest.save())
+            .then(() => res.status(201).json({message : "Request created", requestCreated : true}))
+            .catch((error) => res.status(500).json({message : error.message, requestCreated : false}))        
+    }
 
-    const {serviceOwnerId, serviceId, serviceApplicantId, ownerState, applicantState, dateRequestService, startRequestService, endRequestService, additionalInfo} = req.body;
-
-    const serviceRequest = new ServiceRequest({serviceOwnerId, serviceId, serviceApplicantId, ownerState, applicantState, dateRequestService, startRequestService, endRequestService, additionalInfo});
+const isValidRequestService = ({dateRequestService, startRequestService, endRequestService, serviceOwnerId, serviceApplicantId}) => 
+    new Promise((resolve, reject) => {
+        isRequestServiceFullField(dateRequestService, startRequestService, endRequestService);
+        isSameUser(serviceOwnerId, serviceApplicantId);
+        isInvalidDate(dateRequestService);
+        return isOverSchedule(startRequestService, endRequestService, dateRequestService)
+            .then(() => resolve())
+            .catch(error => reject(error))
+    })
     
-    try {
-        if(serviceOwnerId === serviceApplicantId) {
-            throw new Error("You cannot request service from yourself")
-        } else if (startRequestService >= endRequestService) {
-            throw new Error("Incorrect shcedule")
-        } else if (Date.now() >= dateRequestService) {
-            throw new Error("Incorrect date")
-        } else {
-            return serviceRequest.save()
-                .then(() => res.status(201).json({message : "Request created", requestCreated : true}))
-                .catch(() => res.status(500).json({message : "Any problem with the database", requestCreated : false}))
-        }  
-    } catch (error) {
-        res.status(500).json({message : error.message, requestCreated : false})
-    }   
+const isRequestServiceFullField = (dateRequestService, startRequestService, endRequestService) => {
+    if(!dateRequestService || !startRequestService || !endRequestService) {
+        throw new Error("Fill in the fields")
+    }
+}
+
+const isSameUser = (serviceOwnerId, serviceApplicantId) => {
+    if(serviceOwnerId.equals(serviceApplicantId)) {
+        throw new Error("You cannot request service from yourself")
+    }
+}
+
+const isInvalidDate = (dateRequestService) => {
+    if (new Date(Date.now()) >= new Date(dateRequestService)) {
+        throw new Error("Incorrect date")
+    } 
+}
+
+
+const isOverSchedule = (startRequestService, endRequestService, dateRequestService) => 
+    new Promise ((resolve, reject) => {
+        ServiceRequest.find({dateRequestService})
+            .then(requests => {
+                const isInvalid = requests.some(request => { 
+                    if (request.startRequestService < endRequestService && request.endRequestService > startRequestService) {
+                        return true;
+                    }
+                })
+                if(isInvalid) {
+                    reject(new Error("Is bussy"))
+                }
+                resolve()
+            })
+    })
+    
+
+exports.obtainServiceRequests = (req, res, next) => {
+    console.log(req.query)
+    return ServiceRequest.find({serviceId : req.query.serviceId})
+        .then(requests => res.status(200).json({requests}))
+        .catch(error => res.status(500).json({message : error.message}))
 }
 
 exports.readUserOwnerRequests = (req, res, next) => {
@@ -51,7 +93,17 @@ exports.confirmOwnerRequest = (req, res, next) => {
             serviceRequest.ownerState = "done"            
             return serviceRequest.save()
         })
-        .then(response => console.log(response._id))
+        .then(response => deleteConfirmedRequest(response))
+        .catch(error => console.log(error))
+}
+
+exports.rejectRequest = (req, res, next) => {
+    ServiceRequest.findById(req.body.id)
+        .then(serviceRequest => {
+            console.log("Hola")
+            serviceRequest.ownerState = "rejected"
+            return serviceRequest.save()
+        })
         .catch(error => console.log(error))
 }
 
@@ -62,16 +114,34 @@ exports.confirmApplicantRequest = (req, res, next) => {
             serviceRequest.applicantState = "done"
             return serviceRequest.save()            
         })
-        .then(response => deleteRequest(response))
+        .then(response => deleteConfirmedRequest(response))
         .catch(error => console.log(error))
 }
 
-deleteRequest = (request) => {
+exports.deleteRequestRejected = (req, res, next) => {
+    console.log(req.body)
+    ServiceRequest.findById(req.body.id)
+        .then(request => deleteRequest(request))
+        .catch(error => console.log(error)) 
+
+}
+
+const deleteConfirmedRequest = (request) => {
+
+    const subtract = new Date("2000-01-01T" + request.endRequestService + ":00") - new Date("2000-01-01T" + request.startRequestService + ":00")
+    const workedHours = new Date(subtract).getHours()
+
     if(request.ownerState === "done" && request.applicantState === "done") {
+        deleteRequest(request)
+    }
+}
+
+const deleteRequest = (request) => {
+    console.log(request)
         ServiceRequest.deleteOne({_id : request._id})
             .then(response => console.log(response))
             .catch(error => console.log(error))
-    }
 }
+
 
 
